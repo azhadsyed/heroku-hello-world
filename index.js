5b2d82e4b2f0dd53e7bcf1499b7d110bbfa06ba3
azhadsyed <azhadsyed17@gmail.com>
2021-01-18 12:09:18 -0500

    resolve dependency gunk

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 require("dotenv").config();
 
 const express = require("express");
+const path = require("path");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
